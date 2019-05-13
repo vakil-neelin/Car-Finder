0ecf63382556c7f95c876cc2fdf78f06f33dc96a
vakil-neelin <vakilneelin@gmail.com>
2019-05-13 19:54:41 -0400

    Added initial files
    Added the chromedriver.exe and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Car-Finder
+
+This project is to automatically search the local Craig List vehicle page for car within a specified list of search parameters. This will be done using Python utilizing selenium to automate multiple web browsers in order to increase the efficiency and time.
+
+## To Do List ##
+- [ ] Add search code
+- [ ] Add search parameter text file
+- [ ] Add report creation
--- car_finder.py
+++ car_finder.py
@@ -7,33 +7,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
-chromedriver_location = r'C:\Users\MRET229\Downloads\chromedriver.exe'
+chromedriver_location = r'chromedriver.exe'
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+# options.add_argument('--headless')
+
 # this open up a blank useable automated chrome window
 driver = webdriver.Chrome(chromedriver_location, chrome_options=options)
 
 driver.get("https://raleigh.craigslist.org/search/cta")
-sleep(1)
-elem = driver.find_element_by_id('searchBox')
 
-elem.clear()
-elem.send_keys('CAT00058')
-sleep(1)
-elem.send_keys(Keys.RETURN)
 
-try:
-    # Find the first result of the search box and click it
-    sleep(.5)
-    elem = WebDriverWait(driver, 300).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="navContainerCollapse"]/div/ul/li[2]/a/div[1]')))
-    sleep(1)
-    elem.click()
-except:
-    print "Failed Start up"
-    pass
 
-WebDriverWait(driver, 240).until(
-    EC.invisibility_of_element_located((By.ID, 'noSearch')))
-sleep(.5)
-
+# sleep(1)
+# elem = driver.find_element_by_id('searchBox')
+#
+# elem.clear()
+# elem.send_keys('CAT00058')
+# sleep(1)
+# elem.send_keys(Keys.RETURN)
+#
+# try:
+#     # Find the first result of the search box and click it
+#     sleep(.5)
+#     elem = WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="navContainerCollapse"]/div/ul/li[2]/a/div[1]')))
+#     sleep(1)
+#     elem.click()
+# except:
+#     print "Failed Start up"
+#     pass
+#
+# WebDriverWait(driver, 240).until(EC.invisibility_of_element_located((By.ID, 'noSearch')))
+# sleep(.5)
