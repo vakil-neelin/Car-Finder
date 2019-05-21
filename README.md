7af9356fb4dcbdff429ba9ffa7b58af4d6f8fbd4
vakil-neelin <vakilneelin@gmail.com>
2019-05-20 21:50:22 -0400

    Updated car_finder.py
    Added code to read in .ini files in order to hold a list of cars to search. Made the code more modular and able to be later split into multiple processes. Updated the README to check off the two items on the to do list.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This project is to automatically search the local Craig List vehicle page for car within a specified list of search parameters. This will be done using Python utilizing selenium to automate multiple web browsers in order to increase the efficiency and time.
 
 ## To Do List ##
-- [ ] Add search code
-- [ ] Add search parameter text file
+- [X] Add search code
+- [X] Add search parameter text file
 - [ ] Add report creation
--- car_finder.py
+++ car_finder.py
@@ -7,63 +7,187 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
+import ConfigParser
+
+class CarFinder:
+
+    def __init__(self):
+        # Reads in the list of vehicles
+        self.car_list = self.read_in_config()
+
+        return
+
+    # -------------------------------------------------------
+    # Function Name: read_in_config
+    # Description: Reads in the config file and parses the fields into a list
+    # -------------------------------------------------------
+    @staticmethod
+    def read_in_config():
+        list_of_vehicles = []
+        config = ConfigParser.ConfigParser()
+        config.read('car_parameters.ini')
+
+        for section in config.sections():
+            parameter_list = [section]
+            parameter_dict = {}
+
+            # Adds the Search
+            try:
+                search = config.get(section, 'Search')
+                parameter_dict['Search'] = str(search)
+            except:
+                parameter_dict['Search'] = ''
+
+            # Adds the Beginning Year
+            try:
+                begining_year = config.get(section, 'BeginningYear')
+                parameter_dict['BeginningYear'] = str(begining_year)
+            except:
+                parameter_dict['BeginningYear'] = ''
+
+            # Adds the Ending Year
+            try:
+                ending_year = config.get(section, 'EndingYear')
+                parameter_dict['EndingYear'] = str(ending_year)
+            except:
+                parameter_dict['EndingYear'] = ''
+
+            # Adds the Start Price
+            try:
+                start_price = config.get(section, 'StartPrice')
+                parameter_dict['StartPrice'] = str(start_price)
+            except:
+                parameter_dict['StartPrice'] = ''
+
+            # Adds the End Price
+            try:
+                end_price = config.get(section, 'EndPrice')
+                parameter_dict['EndPrice'] = str(end_price)
+            except:
+                parameter_dict['EndPrice'] = ''
+
+            # Adds the dictionary to the list with the section name
+            parameter_list.append(parameter_dict)
+            # Adds the parameter list to the overall list of vehicles
+            list_of_vehicles.append(parameter_list)
+
+        return list_of_vehicles
+
+    @staticmethod
+    def start_chrome_driver():
+
+        # Loads the chromedriver from the local directory
+        chromedriver_location = os.path.realpath("chromedriver")
+        options = webdriver.ChromeOptions()
+        # options.add_argument('--headless')
+
+        # this open up a blank useable automated chrome window
+        driver = webdriver.Chrome(executable_path=chromedriver_location, chrome_options=options)
+        # Goes to the Local Craiglist vehicle page
+        driver.get("https://raleigh.craigslist.org/search/cta")
+
+        return driver
+
+    #-------------------------------------------------------
+    # Function Name: set_min_price
+    # Description: Sets the minimum price for the vehicle
+    #-------------------------------------------------------
+    @staticmethod
+    def set_min_price(driver, price):
+        elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[4]/input[1]')
+        elem.clear()
+        elem.send_keys(str(price))
+        return
+
+    # -------------------------------------------------------
+    # Function Name: set_max_price
+    # Description: Sets the maximum price for the vehicle
+    # -------------------------------------------------------
+    @staticmethod
+    def set_max_price(driver, price):
+        elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[4]/input[2]')
+        elem.clear()
+        elem.send_keys(str(price))
+        return
+
+    # -------------------------------------------------------
+    # Function Name: set_min_year
+    # Description: Sets the minimum year for the vehicle
+    # -------------------------------------------------------
+    @staticmethod
+    def set_min_year(driver, year):
+        elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[6]/div[1]/input[1]')
+        elem.clear()
+        elem.send_keys(str(year))
+        return
+
+    # -------------------------------------------------------
+    # Function Name: set_max_year
+    # Description: Sets the maximum year for the vehicle
+    # -------------------------------------------------------
+    @staticmethod
+    def set_max_year(driver, year):
+        elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[6]/div[1]/input[2]')
+        elem.clear()
+        elem.send_keys(str(year))
+        return
+
+    # -------------------------------------------------------
+    # Function Name: set_search
+    # Description: Inputs the search parameter
+    # -------------------------------------------------------
+    @staticmethod
+    def set_search(driver, search_parameter):
+        elem = driver.find_element_by_xpath('//*[@id="query"]')
+        elem.clear()
+        elem.send_keys(str(search_parameter))
+        return
+
+    # -------------------------------------------------------
+    # Function Name: start_search
+    # Description: Clicks the search button to make the search
+    # -------------------------------------------------------
+    @staticmethod
+    def start_search(driver):
+        elem = WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchform"]/div[1]/button/span[1]')))
+        elem.click()
+        return
+
+    # -------------------------------------------------------
+    # Function Name: search_for_vehicle
+    # Description:
+    # -------------------------------------------------------
+    def search_for_vehicle(self, driver, input_list):
+
+        min_year = input_list[1]['BeginningYear']
+        max_year = input_list[1]['EndingYear']
+        min_price = input_list[1]['StartPrice']
+        max_price = input_list[1]['EndPrice']
+        search_param = input_list[1]['Search']
+
+        self.set_min_year(driver, min_year)
+        self.set_max_year(driver, max_year)
+        self.set_min_price(driver, min_price)
+        self.set_max_price(driver, max_price)
+        self.set_search(driver, search_param)
+        self.start_search(driver)
+
+        return
+
+    # -------------------------------------------------------
+    # Function Name: search_all_vehicles
+    # Description: Starts a new browser for each vehicle to search
+    # -------------------------------------------------------
+    def search_all_vehicles(self):
+        for vehicle in self.car_list:
+            chrome_driver = self.start_chrome_driver()
+            self.search_for_vehicle(chrome_driver, vehicle)
+
+        return
 
 
-chromedriver_location = os.path.realpath("chromedriver")
-options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-
-# this open up a blank useable automated chrome window
-driver = webdriver.Chrome(executable_path=chromedriver_location, chrome_options=options)
-
-driver.get("https://raleigh.craigslist.org/search/cta")
-
-
-def set_min_price(driver, price):
-    elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[4]/input[1]')
-    elem.clear()
-    elem.send_keys(str(price))
-    return
-
-def set_max_price(driver, price):
-    elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[4]/input[2]')
-    elem.clear()
-    elem.send_keys(str(price))
-    return
-
-def set_min_year(driver, year):
-    elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[6]/div[1]/input[1]')
-    elem.clear()
-    elem.send_keys(str(year))
-    return
-
-def set_max_year(driver, year):
-    elem = driver.find_element_by_xpath('//*[@id="searchform"]/div[2]/div/div[6]/div[1]/input[2]')
-    elem.clear()
-    elem.send_keys(str(year))
-    return
-
-def set_search(driver, search_parameter):
-    elem = driver.find_element_by_xpath('//*[@id="query"]')
-    elem.clear()
-    elem.send_keys(str(search_parameter))
-    return
-
-def start_search(driver):
-    elem = WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchform"]/div[1]/button/span[1]')))
-    elem.click()
-    return
-
-set_min_price(driver, 1000)
-set_max_price(driver, 20000)
-set_min_year(driver, 1990)
-set_max_year(driver, 2000)
-set_search(driver, "Nissan GTR")
-start_search(driver)
-
-
-
-
+c = CarFinder()
+c.search_all_vehicles()
 
 
 
